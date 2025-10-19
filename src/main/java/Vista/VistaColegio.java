8a3f785fa5cc8e6a4c3dac5241c6a6e4928043a5
scharris7 <scharris2007@gmail.com>
2025-10-19 15:15:36 -0500

    Se terminó la clase VistaColegio dentro del paquete Vista

--- src/main/java/Vista/VistaColegio.java
+++ src/main/java/Vista/VistaColegio.java
@@ -3,14 +3,35 @@
  */
 
 package Vista;
+import Modelo.Colegio;
+import javax.swing.JOptionPane;
 
 /**
  *
  * @author schar
  */
 public class VistaColegio {
-
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Colegio col = new Colegio(10, 10);
+        int op;
+
+        do {
+            op = Integer.parseInt(JOptionPane.showInputDialog(
+                "MENÚ COLEGIO\n" +
+                "1. Registrar Profesor\n" +
+                "2. Registrar Estudiante\n" +
+                "3. Reporte de Estudiantes\n" +
+                "4. Reporte de Profesores\n" +
+                "5. Salir"));
+
+            switch (op) {
+                case 1: col.registrarProfesor(); break;
+                case 2: col.registrarEstudiante(); break;
+                case 3: col.reporteEstudiantes(); break;
+                case 4: col.reporteProfesores(); break;
+                case 5: JOptionPane.showMessageDialog(null, "Saliendo..."); break;
+                default: JOptionPane.showMessageDialog(null, "Opción no válida");
+            }
+        } while (op != 5);
     }
 }
