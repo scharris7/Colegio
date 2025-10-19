63970a21444c111d88feecc4bf736c441c82633d
scharris7 <scharris2007@gmail.com>
2025-10-19 15:06:24 -0500

    Se creo la clase VistaColegio dentro del paquete Vista

---
+++ src/main/java/Vista/VistaColegio.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package Vista;
+
+/**
+ *
+ * @author schar
+ */
+public class VistaColegio {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
